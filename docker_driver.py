942de9f1bcf254b1c5d813d7e4d4bb8256cd31c1
amuhebwa <muhebwa.grey@gmail.com>
2019-10-21 22:48:03 -0400

    changed the structure of the docker driver python file

--- docker_driver.py
+++ docker_driver.py
@@ -1,14 +1,26 @@
 import os
-# mycmd1 = os.popen('docker run -v "$PWD":/usr/local/src/app -p 8080:8080 amuhebwa/confluence_prediagnostics').read()
+import code
 cmd1 = 'docker run -v "$PWD":/usr/local/src/app -p 8080:8080 amuhebwa/confluence_prediagnostics'
 cmd2 = 'docker run  -v "$PWD":/usr/local/src/app -p 8081:8081 amuhebwa/bam_algorithm'
 cmd3 = 'docker run  -v "$PWD":/usr/local/src/app -p 8082:8082 amuhebwa/confluence_integrator'
 cmd4 = 'docker run  -v "$PWD":/usr/local/src/app -p 8083:8083 amuhebwa/confluence_postdiagnostics'
+
+#list_of_commands = [cmd1, cmd2, cmd3, cmd4]
+#code.interact(local=locals())
+print("************* STARTING PRE-DIAGNOSTICS ***************")
 os.popen(cmd1).read()
-print("=================================================================================")
+print("************* PRE-DIAGNOSTICS COMPLETED ***************")
+
+print("************* STARTING BAM COMPUTATIONS ***************")
 os.popen(cmd2).read()
-print("=================================================================================")
+print("************* BAM COMPUTATIONS COMPLETED ***************")
+
+print("************* STARTING INTEGRATOR CALCULATIONS **************")
 os.popen(cmd3).read()
-print("=================================================================================")
+
+print("************* INTEGRATOR CALCULATIONS COMPLETED ***************")
+
+print("************* STARTING POST-DIAGNOSTICS ***************")
 os.popen(cmd4).read()
-print("***********TADAAAAAAAAAAAAAA***********************")
+print("************* POST-DIAGNOSTICS COMPLETED ***************")
+
